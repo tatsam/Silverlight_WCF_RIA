69557face352fd5bb166cdeb29abb53574062b54
struggleendlessly <struggleendlessly@gmail.com>
2013-11-20 20:10:23 +0200

    Part2

---
+++ TasksManager.Web/TasksDomainService.cs
@@ -0,0 +1,48 @@
+﻿
+namespace TasksManager.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // Реализует логику приложения с использованием контекста TaskManagerEntities.
+    // TODO: добавьте свою прикладную логику в эти или другие методы.
+    // TODO: включите проверку подлинности (Windows/ASP.NET Forms) и раскомментируйте следующие строки, чтобы запретить анонимный доступ
+    // Кроме того, рассмотрите возможность добавления ролей для соответствующего ограничения доступа.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public class TasksDomainService : LinqToEntitiesDomainService<TaskManagerEntities>
+    {
+
+        // TODO:
+        // рассмотрите возможность сокращения результатов метода запроса.  Если необходим дополнительный ввод,
+        // то в этот метод можно добавить параметры или создать дополнительные методы выполнения запроса с другими именами.
+        // Для поддержки разбиения на страницы добавьте упорядочение в запрос "Tasks".
+        public IQueryable<Task> GetTasks()
+        {
+            return this.ObjectContext.Tasks;
+        }
+
+        public Task GetTask(int taskId)
+        {
+            return this.ObjectContext.Tasks.FirstOrDefault(t => t.TaskId == taskId);
+        }
+
+        public IQueryable<Task> GetTasksByStartDate(
+        DateTime lowerDateTimeInclusive,
+        DateTime upperDateTimeInclusive)
+        {
+            return this.ObjectContext.Tasks.Where(
+                t => t.StartDate >= lowerDateTimeInclusive && t.StartDate <= upperDateTimeInclusive);
+        }
+    }
+}
+
+
---
+++ TasksManager/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.DomainServices.Client;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TasksManager.Web;
+
+namespace TasksManager
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void taskDomainDataSource_LoadedData(object sender, System.Windows.Controls.LoadedDataEventArgs e)
+        {
+
+            if (e.HasError)
+            {
+                System.Windows.MessageBox.Show(e.Error.ToString(), "Load Error", System.Windows.MessageBoxButton.OK);
+                e.MarkErrorAsHandled();
+            }
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime lowerDateVal;
+            DateTime upperDateVal;
+            GetDates(out lowerDateVal, out upperDateVal);
+
+            TasksDomainContext context = new TasksDomainContext();
+            taskDataGrid.ItemsSource = context.Tasks;
+            EntityQuery<Task> query = context.GetTasksByStartDateQuery(lowerDateVal, upperDateVal);
+            LoadOperation<Task> loadOp = context.Load(query);
+        }
+
+        private void GetDates(out DateTime lowerDateVal, out DateTime upperDateVal)
+        {
+            lowerDateVal = DateTime.MinValue;
+            upperDateVal = DateTime.MaxValue;
+            if (!string.IsNullOrWhiteSpace(lowerDate.Text))
+            {
+                lowerDateVal = DateTime.Parse(lowerDate.Text);
+            }
+            if (!string.IsNullOrWhiteSpace(upperDate.Text))
+            {
+                upperDateVal = DateTime.Parse(upperDate.Text);
+            }
+        }
+
+    }
+}
